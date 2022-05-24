e89a568173ab0009816e5f23cc87d6350774d4c3
Pius Osuji <piusosuji9@gmail.com>
2022-05-25 08:24:19 +1200

    This fixes #19

--- src/components/molecules/HomeServices.js
+++ src/components/molecules/HomeServices.js
@@ -43,7 +43,11 @@ export default function Services() {
           </Grid>
         </Grid>
       </Container>
-      <Container>
+      <Container
+        sx={{
+          mb: 10,
+        }}
+      >
         <Grid
           container
           sx={{
