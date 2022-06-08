f370f63253b6c52fe9c41b45689893d2664621b9
Pius Osuji <piusosuji9@gmail.com>
2022-06-09 05:45:20 +1200

    page animation

--- src/components/sections/HomeFeatures.js
+++ src/components/sections/HomeFeatures.js
@@ -1,51 +1,59 @@
 import React from "react";
-import { Container, Box } from "@mui/material";
+import Container from "@mui/material/Container";
 import HomeInfoCard from "../molecules/HomeInfoCard";
 import HomeReverseInfoCard from "../molecules/HomeReverseInfoCard";
 import visit from "./../../assets/visit.svg";
 import investigate from "./../../assets/investigate.svg";
 import form from "./../../assets/form.svg";
 import HomeHeading from "../atoms/HomeHeading";
+import { FadeIn } from "./../atoms/FadeIn.js";
 
 export default function Features() {
   return (
     <>
-      <Box
-        sx={{
+      <div
+        style={{
           width: "100vw",
           background: "#222f43",
           margin: "0",
           padding: "0",
           boxSizing: "border-box",
-          position: "absolute",
-          top: { lg: "40em", md: "40em", xs: "49em" },
         }}
       >
         <Container
           sx={{
             height: "auto",
+            mt: 10,
             py: 5,
           }}
         >
-          <HomeHeading name="How It Works" color={"#FF5C00"} />
-          <HomeInfoCard
-            img={visit}
-            title="Visit our Website"
-            info=" Visit findmycar.netlify.app
+          <FadeIn>
+            <HomeHeading name="How It Works" color={"#FF5C00"} />
+          </FadeIn>
+          <FadeIn>
+            <HomeInfoCard
+              img={visit}
+              title="Visit our Website"
+              info=" Visit findmycar.netlify.app
             to proceed with your vehicle theft report. Our agents are available 24/7 to receive your complaint. The sooner you file a report the higher the chances of recovery"
-          />
-          <HomeReverseInfoCard
-            img={form}
-            title="File a report"
-            info="Fill the Vehicle Theft report form and provide the required details. The vehicle details should be as accurate as possible. The more specific and accurate the details are , the higher the chances of recovery"
-          />
-          <HomeInfoCard
-            img={investigate}
-            title="The police does the rest"
-            info="Your Vehicle Theft Report would be immediately forwarded to the police and other relevant security agencies in the area. Upon recovery we would contact you through the contact details you made available to us"
-          />
+            />
+          </FadeIn>
+          <FadeIn>
+            <HomeReverseInfoCard
+              img={form}
+              title="File a report"
+              info="Fill the Vehicle Theft report form and provide the required details. The vehicle details should be as accurate as possible. The more specific and accurate the details are , the higher the chances of recovery"
+            />
+          </FadeIn>
+          <FadeIn>
+            <HomeInfoCard
+              img={investigate}
+              title="The police does the rest"
+              info="Your Vehicle Theft Report would be immediately forwarded to the police and other relevant security agencies in the area. Upon recovery we would contact you through the contact details you made available to us"
+            />
+          </FadeIn>
         </Container>
-      </Box>
+      </div>
     </>
   );
 }
--- src/components/sections/HomeHero.js
+++ src/components/sections/HomeHero.js
@@ -9,6 +9,7 @@ import heroImage4 from "./../../assets/heroImage4.png";
 import HomeButton from "../atoms/HomeButton";
 import Link from "@mui/material/Link";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { FadeIn } from "./../atoms/FadeIn.js";
 
 // Import Swiper styles
 import "swiper/css";
@@ -37,8 +38,7 @@ export default function HomeHero() {
     <>
       <Box
         sx={{
-          position: "fixed",
-          height: { xs: "auto", md: "auto", lg: "90vh", xl: "80vh" },
+          height: { xs: "auto", md: "auto", lg: "80vh", xl: "80vh" },
           width: "100vw",
           background: "#0a192f",
           display: "flex",
@@ -46,7 +46,7 @@ export default function HomeHero() {
           alignItems: { xs: "right", md: "center" },
           justifyContent: "space-around",
           px: { xs: "15px" },
-          py: 12,
+          pt: 12,
         }}
       >
         <Box
@@ -65,22 +65,24 @@ export default function HomeHero() {
             mr: "2em",
           }}
         >
-          <Typography
-            variant="h1"
-            sx={{
-              color: "#ff5c00",
-              fontWeight: "bold",
-              fontSize: { xs: "1.8em", lg: "2.2em", xl: "2.7em" },
-              fontFamily: "Mochiy Pop One",
-              letterSpacing: { xs: "5px", lg: "15px" },
-              overflowY: "hidden",
-              mt: "1em",
-              alignSelf: { xs: "center", lg: "flex-start" },
-              lineHeight: "1.5em",
-            }}
-          >
-            Recover Your Stolen Car
-          </Typography>
+          <FadeIn>
+            <Typography
+              variant="h1"
+              sx={{
+                color: "#ff5c00",
+                fontWeight: "bold",
+                fontSize: { xs: "1.8em", lg: "2.2em", xl: "2.7em" },
+                fontFamily: "Mochiy Pop One",
+                letterSpacing: { xs: "5px", lg: "15px" },
+                overflowY: "hidden",
+                mt: "1em",
+                alignSelf: { xs: "center", lg: "flex-start" },
+                lineHeight: "1.5em",
+              }}
+            >
+              Recover Your Stolen Car
+            </Typography>
+          </FadeIn>
           <Box
             sx={{
               maxWidth: "22em",
@@ -138,19 +140,21 @@ export default function HomeHero() {
               </SwiperSlide>
             </Swiper>
           </Box>
-          <Typography
-            variant="h4"
-            sx={{
-              color: "#e6e6e6",
-              fontFamily: "Mochiy Pop One",
-              lineHeight: "1.5em",
-              fontSize: { xs: "1.5em", xl: "1.em" },
-              mt: "1.5em",
-            }}
-          >
-            Get your <span style={{ color: "#ff5c00" }}>stolen</span> vehicle
-            back fast with the best Vehicle Theft Recovery System
-          </Typography>
+          <FadeIn>
+            <Typography
+              variant="h4"
+              sx={{
+                color: "#e6e6e6",
+                fontFamily: "Mochiy Pop One",
+                lineHeight: "1.5em",
+                fontSize: { xs: "1.5em", xl: "1.em" },
+                mt: "1.5em",
+              }}
+            >
+              Get your <span style={{ color: "#ff5c00" }}>stolen</span> vehicle
+              back fast with the best Vehicle Theft Recovery System
+            </Typography>
+          </FadeIn>
           <Link
             component={RouterLink}
             to="/"
@@ -159,14 +163,16 @@ export default function HomeHero() {
               textDecoration: "none",
             }}
           >
-            <HomeButton
-              text={"Recover"}
-              style={{
-                padding: "1em 5em",
-                margin: "2em 0",
-                letterSpacing: "5px",
-              }}
-            />
+            <FadeIn>
+              <HomeButton
+                text={"Recover"}
+                style={{
+                  padding: "1em 5em",
+                  margin: "2em 0",
+                  letterSpacing: "5px",
+                }}
+              />
+            </FadeIn>
           </Link>
         </Box>
 
